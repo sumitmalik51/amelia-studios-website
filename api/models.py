d9d8db43ec1bee54bb4789ea45f8aabe277076af
kobbok <kate.oboyle@icloud.com>
2023-04-17 17:43:04 +0100

    refactor: Amended Project_Asset model with new 'content_url_number' field and method

--- api/models.py
+++ api/models.py
@@ -1,23 +1,35 @@
 from django.db import models
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+
 
 # Create your models here.
 class Project(models.Model):
     homepage = models.BooleanField(default=False)
-    category = models.CharField(max_length=100, choices=[('music', 'Music'), ('commercial', 'Commercial')])
+    category = models.CharField(max_length=100, choices=[(
+        'music', 'Music'), ('commercial', 'Commercial')])
     client_name = models.CharField(max_length=100)
     project_title = models.CharField(max_length=100)
     project_type = models.CharField(max_length=100)
     description = models.TextField()
 
-    # Class for how the Project Object will look on DB: 
+    # Method for how the Project Object will look on DB:
     def __str__(self):
         return f"{self.client_name} - {self.project_title}"
-    
-class Project_Asset(models.Model):
-    project = models.ForeignKey(Project, related_name='assets', on_delete=models.CASCADE)
-    content_url = models.CharField(max_length=255)
-    content_description = models.TextField()
 
-    # Class for how the Project Object will look on DB: 
+
+class Project_Asset(models.Model):
+    project = models.ForeignKey(
+        Project, related_name='assets', on_delete=models.CASCADE)
+    content_url = models.URLField(validators=[URLValidator()])
+    content_url_number = models.PositiveIntegerField(null=False, blank=False)
+    content_description = models.TextField(max_length=500)
+
+    # Method for how the Project Object will look on DB:
     def __str__(self):
-        return f"{self.project} - Asset {self.id}"
+        return f"{self.project} - Asset {self.content_url_number}"
+
+    # Method for checking if the content_url_number field is between 1 and 10, if not, raises ValidationError
+    def clean(self):
+        if not 1 <= self.content_url_number <= 10:
+            raise ValidationError('Content URL number must be between 1 and 10.')
