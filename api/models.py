8811f5778b65311eb14bd23fb3a08ce20813a929
kobbok <kate.oboyle@icloud.com>
2023-04-17 14:31:42 +0100

    refactor: Update api model so object is titled by client and project in database view

--- api/models.py
+++ api/models.py
@@ -5,9 +5,13 @@ class Project(models.Model):
     homepage = models.BooleanField(default=False)
     category = models.CharField(max_length=100, choices=[('music', 'Music'), ('commercial', 'Commercial')])
     client_name = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
+    project_title = models.CharField(max_length=100)
     project_type = models.CharField(max_length=100)
     description = models.TextField()
+
+    # Class for how the Project Object will look on DB: 
+    def __str__(self):
+        return f"{self.client_name} - {self.project_title}"
     
 class Project_Asset(models.Model):
     project = models.ForeignKey(Project, related_name='assets', on_delete=models.CASCADE)
@@ -31,3 +35,4 @@ class Project_Asset(models.Model):
     content_8_description = models.TextField(blank=True)
     content_9_url = models.CharField(max_length=255, blank=True)
     content_9_description = models.TextField(blank=True)
+
