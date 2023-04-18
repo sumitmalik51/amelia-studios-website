2a41c91ab068c971aa462f82a2decd5e768a77eb
kobbok <kate.oboyle@icloud.com>
2023-04-18 13:39:02 +0100

    style: Project info basic styling

--- api/models.py
+++ api/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
+from django.core.files import File
+import os
+import urllib.request
+import requests
 
 
 # Create your models here.
@@ -28,8 +32,36 @@ class Project_Asset(models.Model):
     # Method for how the Project Object will look on DB:
     def __str__(self):
         return f"{self.project} - Asset {self.content_url_number}"
+    
+    # Method for checking if the content_url file extension is an image file (e.g.jpg, jpeg, png, gif) or a Vimeo URL. 
+    # If it's an image file, it downloads and saves the image file using Django's 'File' class. It's saved locally to the 'MEDIA_ROOT' directory
+    # If it's a Vimeo URL, the Vimeo API requires an access token and is then downloaded and saved to the 'content_url'. 
+    # def save(self, *args, **kwargs):
+    #     # Get the file extension from the URL
+    #     file_extension = os.path.splitext(self.content_url)[1]
 
-    # Method for checking if the content_url_number field is between 1 and 10, if not, raises ValidationError
+    #     # Download the image or video file from the URL and save it to the content_url field
+    #     if file_extension in ['.jpg', '.jpeg', '.png', '.gif']:
+    #         img_name = self.content_url.split("/")[-1]
+    #         img_temp = urllib.request.urlopen(self.content_url)
+    #         self.content_url.save(img_name, File(img_temp), save=False)
+    #     elif 'vimeo.com' in self.content_url:
+    #         # For Vimeo URLs, extract the Vimeo video ID from the URL
+    #         vimeo_id = self.content_url.split('/')[-1]
+    #         # Fetch the Vimeo video info using Vimeo API with an access token
+    #         access_token = 'YOUR_VIMEO_ACCESS_TOKEN' # Replace with your Vimeo access token
+    #         headers = {'Authorization': 'Bearer ' + access_token}
+    #         response = requests.get('https://api.vimeo.com/videos/' + vimeo_id, headers=headers)
+    #         video_info = response.json()
+    #         video_url = video_info['files'][0]['link']
+    #         video_name = video_url.split("/")[-1]
+    #         video_temp = urllib.request.urlopen(video_url)
+    #         self.content_url.save(video_name, File(video_temp), save=False)
+
+    #     super().save(*args, **kwargs)
+
+
+    # Method for checking if the content_url_number field is between 1 and 10 & if not it raises ValidationError
     def clean(self):
         if not 1 <= self.content_url_number <= 10:
             raise ValidationError('Content URL number must be between 1 and 10.')
--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import project_list
+# from django.conf import settings
+# from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('projects/', project_list, name='project-list'),
 ]
+# ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
