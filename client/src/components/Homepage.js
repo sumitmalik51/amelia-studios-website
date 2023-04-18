83436543e6bc4d59e337ee453f98b636339f5321
kobbok <kate.oboyle@icloud.com>
2023-04-18 14:57:34 +0100

    feat: Dropbox images test, images from ameliastudios not working

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import Swiper from 'swiper/react'
+
+
 
 const Homepage = () => {
 
@@ -39,12 +42,16 @@ const Homepage = () => {
             :           
             <div className='project-wrapper'>
               {projects.map(project => {
-                const { id, content_url, content_description, client_name, project_title, project_type, description } = project
+                const { id, assets, client_name, project_title, project_type, description } = project
                 console.log(project)
                 return (
                   <div className='project-container' key={id}>
                     <div className='swiper-wrapper'>
-                      <img src={content_url} alt={`${content_description}`} />
+                    {assets.map(asset => (
+                        <div key={asset.url}>
+                          <img src={asset.url} alt={asset.description} />
+                        </div>
+                      ))}
                     </div>
                     <div className='project-info'>
                       <h4>{client_name}</h4>
@@ -64,57 +71,3 @@ const Homepage = () => {
 
 export default Homepage
 
-// import { useState, useEffect } from 'react'
-// import axios from 'axios'
-
-
-
-// const Homepage = () => {
-
-//   const [projects, setProjects] = useState([])
-//   const [errors, setErrors] = useState(false)
-//   const [loading, setLoading] = useState(true)
-
-//   // accessing Express API to get all collaborations
-//   useEffect(() => {
-//     const getProjects = async () => {
-//       try {
-//         const { data } = await axios.get('/api/projects/')
-//         setProjects(data)
-//       } catch (err) {
-//         console.log(err)
-//         setErrors(true)
-//       }
-//       setLoading(false)
-//     }
-//     getProjects()
-//   }, [])
-
-
-//   return (
-//     <div className='project-wrapper'>
-// {
-//   loading ?
-//     <span>Loading...</span>
-//     :
-//     errors ?
-//       <span>Collaborations could not load. Please try again later!</span>
-//       :
-//           <div className='project-container'>
-//             {projects.map(project => {
-//               const { id, url, description } = project
-//               console.log(project)
-//               return (
-//                 <div className='viewport' key={id}>
-//                   <img src={url} alt={description} className='img-thumbnail' />
-//                 </div>
-//               )
-//             })}
-//           </div>
-//       }
-//     </div>
-//   )
-// }
-
-
-// export default Homepage
