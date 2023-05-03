cb558654683e7efce5a55dd7dd08700ce95ec897
kobbok <kate.oboyle@icloud.com>
2023-05-03 09:57:25 +0100

    refactor: Updates to header.js and header.scss

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,54 +1,20 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Logo from '../assets/logos/logo-title.png'
 import { Link } from "react-router-dom";
 
 const Header = () => {
-  const [showMusicDropdown, setShowMusicDropdown] = useState(false)
-  const [showCommercialsDropdown, setShowCommercialsDropdown] = useState(false)
-
-  // toggle music dropdown visibility
-  const handleMusicClick = () => {
-    setShowMusicDropdown(!showMusicDropdown)
-  }
-
-  // toggle commercials dropdown visibility
-  const handleCommercialsClick = () => {
-    setShowCommercialsDropdown(!showCommercialsDropdown)
-  }
 
   return (
     <header className="navbar">
-      <div className="navbar-left">
-        <Link to="/" className="navbar-contact">
-          <img src={Logo} alt="Logo" className="navbar-logo" />
+      <div className="navbar__left">
+        <Link to="/">
+          <img src={Logo} alt="Logo" className="navbar__logo" />
         </Link>
-        <div className="navbar-title" onClick={handleMusicClick}>
-          Music
-          {showMusicDropdown && (
-            <div className="navbar-dropdown">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
-              <a href="#">Link 4</a>
-            </div>
-          )}
-        </div>
-        <div className="navbar-title" onClick={handleCommercialsClick}>
-          Commercials
-          {showCommercialsDropdown && (
-            <div className="navbar-dropdown">
-              <a href="#">Link 1</a>
-              <a href="#">Link 2</a>
-              <a href="#">Link 3</a>
-              <a href="#">Link 4</a>
-            </div>
-          )}
-        </div>
       </div>
-      <div className="navbar-right">
-        <div className="navbar-title">
+      <div className="navbar__right">
+        <div className="navbar__title">
           {/* <Link to="/contact" className="navbar-contact"> */}
-            Contact
+            Menu
           {/* </Link> */}
         </div>
       </div>
@@ -57,3 +23,68 @@ const Header = () => {
 }
 
 export default Header
+
+
+
+
+
+
+// import React, { useState } from 'react'
+// import Logo from '../assets/logos/logo-title.png'
+// import { Link } from "react-router-dom";
+
+// const Header = () => {
+//   const [showMusicDropdown, setShowMusicDropdown] = useState(false)
+//   const [showCommercialsDropdown, setShowCommercialsDropdown] = useState(false)
+
+//   // toggle music dropdown visibility
+//   const handleMusicClick = () => {
+//     setShowMusicDropdown(!showMusicDropdown)
+//   }
+
+//   // toggle commercials dropdown visibility
+//   const handleCommercialsClick = () => {
+//     setShowCommercialsDropdown(!showCommercialsDropdown)
+//   }
+
+//   return (
+//     <header className="navbar">
+//       <div className="navbar-left">
+//         <Link to="/" className="navbar">
+//           <img src={Logo} alt="Logo" className="navbar-logo" />
+//         </Link>
+//         <div className="navbar-title" onClick={handleMusicClick}>
+//           Music
+//           {showMusicDropdown && (
+//             <div className="navbar-dropdown">
+//               <a href="#">Link 1</a>
+//               <a href="#">Link 2</a>
+//               <a href="#">Link 3</a>
+//               <a href="#">Link 4</a>
+//             </div>
+//           )}
+//         </div>
+//         <div className="navbar-title" onClick={handleCommercialsClick}>
+//           Commercials
+//           {showCommercialsDropdown && (
+//             <div className="navbar-dropdown">
+//               <a href="#">Link 1</a>
+//               <a href="#">Link 2</a>
+//               <a href="#">Link 3</a>
+//               <a href="#">Link 4</a>
+//             </div>
+//           )}
+//         </div>
+//       </div>
+//       <div className="navbar-right">
+//         <div className="navbar-title">
+//           {/* <Link to="/contact" className="navbar-contact"> */}
+//             Contact
+//           {/* </Link> */}
+//         </div>
+//       </div>
+//     </header>
+//   )
+// }
+
+// export default Header
