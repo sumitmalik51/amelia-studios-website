94f9a0d39876f6a4fad318615428b20da70de483
kobbok <kate.oboyle@icloud.com>
2023-05-09 10:41:25 +0100

    admin: Azure deployment steps for a django postgres web app

--- project/settings.py
+++ project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-steh6iamq5o0rswd!uhf_s*zjknyvssth1#f5+%ffsxh0^p7e6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
