b4913a43b9a924858fd13d250e02d9bafd8da110
kobbok <kate.oboyle@icloud.com>
2023-05-10 21:38:22 +0100

    config

--- project/settings.py
+++ project/settings.py
@@ -31,11 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
-    'https://ameliastudios-python-postgres-234.azurewebsites.net/',
-    ]
+ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
 
 # ADMINS = [('Kate', 'kate.e.oboyle@gmail.com'), ('AS', 'ameliastudios.azure@gmail.com')]
 # MANAGERS = ADMINS
